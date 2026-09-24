cfe404ba33c5f57ff8c26b2315fcb37a99162e7f
powerslave1804 <steeltormentor676@gmail.com>
2026-09-24 12:48:45 +0200

    New additions

--- tests/pom.spec.js
+++ tests/pom.spec.js
@@ -1,6 +1,7 @@
-import { expect, test } from "@playwright/test";
+//import { expect, test } from "@playwright/test"; zamenili smo zbog uvodjenja fixtures
 import { LoginPage } from '../pages/loginPage.js';
 import { ProductsPage } from "../pages/productsPage.js";
+import { expect, test } from "../fixtures/test.js";
 
 const users = [
     {
@@ -28,6 +29,15 @@ test.describe('Login POM tests', () => {
         await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
         await expect(page.locator('.title')).toHaveText('Products')
     })
+
+    //     test('Standard user login fixture', async ({ loginPage }) => {
+
+    //     await loginPage.goto()
+    //     await loginPage.loginAsStandardUser()
+
+    //     await expect(loginPage.page).toHaveURL('https://www.saucedemo.com/inventory.html')
+    //     await expect(loginPage.page.locator('.title')).toHaveText('Products')
+    // })
 
     test('Locked out user cant login', async ({ page }) => {
         const loginPage = new LoginPage(page)
@@ -82,6 +92,19 @@ test.describe('Login POM tests', () => {
         })
     }
 
+    for (const user of users) {
+        test(`Login with ${user.username} fixture`, async ({ loginPage}) => {
+
+            await loginPage.goto()
+            await loginPage.login(
+                user.username,
+                user.password
+            )
+
+            await expect(loginPage.page).toHaveURL('https://www.saucedemo.com/inventory.html')
+        })
+    }
+
     test('Page has correct title', async ({page}) => {
         const loginPage = new LoginPage(page)
 
@@ -106,6 +129,50 @@ test.describe('Login POM tests', () => {
 
         expect(currentUrl).toBe('https://www.saucedemo.com/inventory.html')
     })
+
+    test('Standard user login fixture', async ({ loginPage }) => {
+
+        await loginPage.goto()
+        await loginPage.loginAsStandardUser()
+
+        await expect(loginPage.page).toHaveURL('https://www.saucedemo.com/inventory.html')
+        await expect(loginPage.page.locator('.title')).toHaveText('Products')
+    })
+
+    test('Locked out user cant login fixture', async ({ loginPage}) => {
+        await loginPage.goto()
+        await loginPage.loginAsLockedOutUser()
+
+        await expect(loginPage.errorMessage).toBeVisible()
+        await expect(loginPage.errorMessage).toContainText('Epic sadface: Sorry, this user has been locked out.')
+
+        await expect(loginPage.page).not.toHaveURL('https://www.saucedemo.com/inventory.html')
+    })
+
+    test('User can logout fixture', async ({ loginPage, page}) => {
+        const productPage = new ProductsPage(page)
+
+        await loginPage.goto()
+        await loginPage.loginAsStandardUser()
+
+        await expect(productPage.title).toHaveText('Products')
+
+        await productPage.logout()
+
+        await expect(page).toHaveURL('https://www.saucedemo.com/')
+    })
+
+    test('User can logout fixtures 2', async ({ loginPage, productPage }) => {
+        await loginPage.goto()
+        await loginPage.loginAsStandardUser()
+
+        await expect(productPage.title).toHaveText('Products')
+
+        await productPage.logout()
+
+        await expect(productPage.page).toHaveURL('https://www.saucedemo.com/')
+    })
+    
 })
 
 
